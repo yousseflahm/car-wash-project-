cfadd436a8e1bbb0cbd5ed96d18327caab66b702
youssef lahmad <yousseflahmade@gmail.com>
2025-05-18 20:02:08 +0100

    adding file auth.php and passwords.php to fr

---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de langue pour la réinitialisation du mot de passe
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes suivantes sont les messages par défaut correspondant aux raisons
+    | fournies par le gestionnaire de mots de passe lors d’une tentative de mise
+    | à jour du mot de passe, comme un échec dû à un mot de passe ou un jeton invalide.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Nous avons envoyé le lien de réinitialisation de mot de passe par e-mail.',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation du mot de passe est invalide.',
+    'user' => "Aucun utilisateur n’a été trouvé avec cette adresse e-mail.",
+
+];
