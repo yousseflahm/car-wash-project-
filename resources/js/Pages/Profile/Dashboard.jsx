b877e7f4f5fb571806b17a974d23cba3bb0aaa81
youssef lahmad <yousseflahmade@gmail.com>
2025-11-15 19:45:05 +0100

    fix importing Card.jsx

--- resources/js/Pages/Profile/Dashboard.jsx
+++ resources/js/Pages/Profile/Dashboard.jsx
@@ -9,7 +9,7 @@ import {
   CardDescription,
   CardContent,
   CardFooter,
-} from "@/components/ui/card.jsx";
+} from "@/components/ui/Card.jsx";
 
 export default function Dashboard({ auth, bookings, services }) {
   const [selectedService, setSelectedService] = useState(null);
