2a98ea06354b93445e786746029bdbe6db33f905
youssef lahmad <yousseflahmade@gmail.com>
2025-11-16 20:41:32 +0100

    removing guestlayout from auth washer files

--- resources/js/Pages/washer/Auth/VerifyEmail.jsx
+++ resources/js/Pages/washer/Auth/VerifyEmail.jsx
@@ -1,5 +1,5 @@
 import PrimaryButton from '@/Components/PrimaryButton';
-import GuestLayout from '@/Layouts/GuestLayout';
+
 import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function VerifyEmail({ status }) {
@@ -12,7 +12,7 @@ export default function VerifyEmail({ status }) {
     };
 
     return (
-        <GuestLayout>
+        <>
             <Head title="Email Verification" />
 
             <div className="mb-4 text-sm text-gray-600">
@@ -45,6 +45,6 @@ export default function VerifyEmail({ status }) {
                     </Link>
                 </div>
             </form>
-        </GuestLayout>
+        </>
     );
 }
